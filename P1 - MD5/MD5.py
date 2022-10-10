84585271ded095ad84fbf259c89b3c204b41906e
Samantha <79162978+SamanthaRG@users.noreply.github.com>
2022-10-10 16:33:40 +0200

    Comentarios y estructuración del código.

--- P1 - MD5/MD5.py
+++ P1 - MD5/MD5.py
@@ -20,34 +20,63 @@ def I(X, Y, Z):
 class MD5:
 
     def __init__(self):
-        # Initialize buffers, etc.
+        """
+        Inicializamos los buffers.
+        """
+        self.A = 0
+        self.B = 0
+        self.C = 0
+        self.D = 0
+
         self.length: int = 0
         self.n_filled_bytes: int = 0
         self.buf: bytearray = bytearray(64)
         pass
 
     def _step1(self, msgOriginal):
-        # Append Padding Bits to Message
+        """
+        Añadir bits de relleno al mensasje.
+        :param msgOriginal: mensaje original que cifraremos
+        :return: retorna el mensaje transformado en bits
+        """
+
+        # Codificamos el mensaje de entrada en bits.
         msgBits = bytearray(msgOriginal.encode('utf-8'))
         msgBits.append(0x80)
 
+        # Comprobamos que la longitud del mensaje sea menor que 56 bytes para añadir tantos 0 como sean necesarios hasta que sea igual a 56.
         while (len(msgBits) % 64) < 56:
             msgBits.append(0)
-
         print(msgBits.hex())
+
         return msgBits
 
     def _step2(self, msgOriginal, msgBits):
-        # Append Length of Original Message
-        # ORIG LEN MOD64 -- 56, MSG+LEN
-        msgLen = len(msgOriginal)
-        mod64=msgLen*8 #(len(msg)*8)%(1<<64)) si el mensaje fuera muy grande
-        print(mod64)
-        sumamod=msgBits+mod64.to_bytes(8,byteorder='little')
-        print(sumamod.hex())
-        pass
+        """
+        Agregar longitud del mensaje original.
+        ORIG LEN MOD64 -- 56, MSG+LEN
+        :param msgOriginal: El mensaje que se introduce.
+        :param msgBits: El mensaje que hemos transformado.
+        :return: devuelve la suma en hexadecimal del mensaje
+        """
 
-    def _step3(self, ):
+        # Obtenemos la longitud del mensaje original.
+        msgLen = len(msgOriginal)
+
+        # Multiplicamos la longitud del mensaje original x8 para tener el mensaje en bytes.
+        # Si el mensaje fuera muy grande -> (len(msg)*8)%(1<<64))
+        mod64 = msgLen*8
+
+        print(mod64)
+
+        # Sumamos el mensaje original a la longitud calculada a bytes.
+        sumaMod = msgBits+mod64.to_bytes(8, byteorder='little')
+        print(sumaMod.hex())
+
+        return sumaMod
+
+
+    def _step3(self):
         # Initialize MD Buffer
         # self.state: tuple[int, int, int, int] = (0x67452301, 0xefcdab89, 0x98badcfe, 0x10325476)
         self.A = 0x67452301
@@ -69,7 +98,8 @@ class MD5:
     def __call__(self, data_to_digest):
         # Implements the algorithm by calling each _step* function
         msgBits = self._step1(data_to_digest)
-        self._step2(data_to_digest,msgBits)
+        sumaMod = self._step2(data_to_digest, msgBits)
+
         print('result', data_to_digest)
 
 
